95b01796fa25c1156b4222d33f4cc4bf5f2aeb6b
letuantam <letuantam96@gmail.com>
2025-03-25 15:01:30 +0700

    build - add downloading in splash

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Skybull-SkybullBaseTemplate-0.1.0";
+const contentToCache = [
+    "Build/ChatGPT-PvP.loader.js",
+    "Build/ChatGPT-PvP.framework.js",
+    "Build/ChatGPT-PvP.data",
+    "Build/ChatGPT-PvP.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
