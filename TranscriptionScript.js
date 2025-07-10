1d7c61fb6367ba7242c22429a1d7ac8b82e4c905
ly.dev <ly.nguyenhuong@skybull.studio>
2025-07-10 09:44:06 +0700

    .

--- TranscriptionScript.js
+++ TranscriptionScript.js
@@ -1,18 +1,9 @@
 let recognization = new webkitSpeechRecognition();
-let micPermissionAsked = false;
 
 var result = false;
 var stop = true; // Initially stopped
 var started = false; // Recognition has not started yet
 var GameObjName = null;
-
-async function ensureMicPermission() {
-    if (!micPermissionAsked) {
-        micPermissionAsked = true;
-        await navigator.mediaDevices.getUserMedia({ audio: true });
-    }
-}
-
 
 function SetGameObjectName(name) {
     GameObjName = name;
@@ -20,7 +11,6 @@ function SetGameObjectName(name) {
 }
 
 function runspeechrecognition(listenContinuous) {
-	await ensureMicPermission();
 	
     recognization.onstart = () => {
         console.log("Speech recognition started");
