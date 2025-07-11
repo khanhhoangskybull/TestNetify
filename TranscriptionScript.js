8acd268ee20bb6e04f4dd062594e3a0a04566af1
ly.dev <ly.nguyenhuong@skybull.studio>
2025-07-11 11:07:50 +0700

    fix aspect ratio

--- TranscriptionScript.js
+++ TranscriptionScript.js
@@ -1,18 +1,16 @@
 let recognization = new webkitSpeechRecognition();
-recognization.continuous = false;
-recognization.interimResults = false;
 
 var result = false;
-var stop = true;
-var started = false;
-let listeningSessionActive = true;
+var stop = true; // Initially stopped
+var started = false; // Recognition has not started yet
+var GameObjName = null;
 
-async function runspeechrecognition(listenContinuous) {	
-	if (started){
-		console.log("Recognition already running");
-        return;
-	}
+function SetGameObjectName(name) {
+    GameObjName = name;
+    console.log(GameObjName);
+}
 
+function runspeechrecognition(listenContinuous) {
     recognization.onstart = () => {
         console.log("Speech recognition started");
         started = true;
@@ -24,70 +22,46 @@ async function runspeechrecognition(listenContinuous) {
         var transcript = e.results[0][0].transcript;
         console.log("Speech recognition result:", transcript);
 
-        window.Gameinstance.SendMessage(GameObjName, "OnMicResult", transcript);
+        window.Gameinstance.SendMessage(GameObjName, "Result", transcript);
         result = true;
+
+        if (listenContinuous && !stop) {
+            setTimeout(() => {
+                if (!started) {
+                    recognization.start(); // Restart recognition
+                }
+            }, 100);
+        }
     };
 
     recognization.onend = () => {
         console.log("Speech recognition ended");
         started = false;
-		stop = true;
 
-        window.Gameinstance.SendMessage(GameObjName, "OnMicEnd");
-        
-
-        // if (listenContinuous && listeningSessionActive && !result) {
-            // console.log("Restarting recognition...");
-            // setTimeout(() => {
-                // if (!started && listeningSessionActive) {
-                    // try {
-                        // recognization.start();
-                    // } catch (err) {
-                        // console.error("Error restarting recognition:", err);
-                    // }
-                // }
-            // }, 300);
-        // }
+        if (!result && listenContinuous && !stop) {
+            setTimeout(() => {
+                if (!started) {
+                    recognization.start(); // Restart recognition
+                }
+            }, 100);
+        }
     };
 
-    try {
-        if (!started && stop) {            
-            recognization.start();
-        }
-    } catch (error) {
-        console.error("Recognition start error:", error);
-    }
-}
-
-async function askMicrophonePermission() {
-    try {
-        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-        console.log("Microphone permission granted");
-
-        stream.getTracks().forEach(track => track.stop());
-
-        window.Gameinstance.SendMessage("MicHandler", "OnMicPermissionGranted");
-    } catch (err) {
-        console.error("User denied microphone permission:", err);
-        window.Gameinstance.SendMessage("MicHandler", "OnMicPermissionDenied");
+    // Start recognition if it is not running and has not been stopped
+    if (!started && stop) {
+        stop = false;
+        recognization.start();
     }
 }
 
 function stoprecognition() {
     console.log("Stopping speech recognition");
-    if (started) {
-        recognization.stop();
-    }
+    recognization.stop();
     stop = true;
     started = false;
 }
 
-function SetGameObjectName(name) {
-    GameObjName = name;
-    console.log(GameObjName);
-}
-
-function downloadFile(fileName, content) {
+function downloadfile(fileName, content) {
     const link = document.createElement("a");
     const file = new Blob([content], { type: 'text/plain' });
     link.href = URL.createObjectURL(file);
